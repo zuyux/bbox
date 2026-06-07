86cc1df6be3d93ee09adf9af7681ad9d8623fe0f
fabohax <40230@pm.me>
2026-06-06 19:54:28 -0500

    Encode derived Nostr keys as npub bech32 addresses

--- lib/nostr.ts
+++ lib/nostr.ts
@@ -1,4 +1,5 @@
 import { schnorr } from '@noble/secp256k1';
+import { bech32 } from 'bech32';
 
 function hexToBytes(hex: string): Uint8Array {
   if (hex.length % 2 !== 0) {
@@ -11,14 +12,9 @@ function hexToBytes(hex: string): Uint8Array {
   return bytes;
 }
 
-function bytesToHex(bytes: Uint8Array): string {
-  return Array.from(bytes)
-    .map((byte) => byte.toString(16).padStart(2, '0'))
-    .join('');
-}
-
 export function getNostrPublicKeyFromPrivateKey(privateKeyHex: string): string {
   const privateKey = hexToBytes(privateKeyHex);
   const publicKeyBytes = schnorr.getPublicKey(privateKey);
-  return bytesToHex(publicKeyBytes);
+  const words = bech32.toWords(publicKeyBytes);
+  return bech32.encode('npub', words);
 }
